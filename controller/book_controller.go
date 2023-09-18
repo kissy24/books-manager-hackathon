62fa1cead575f7d581f403478500aa45ca688dbe
kissy24 <kissy105808j@gmail.com>
2023-09-18 15:57:49 +0900

    add front end

--- controller/book_controller.go
+++ controller/book_controller.go
@@ -1,16 +1,46 @@
 package controller
 
 import (
+	"net/http"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 	"github.com/kissy24/go-reading-books/model"
 )
 
-func ShowAllBooks(ctx *gin.Context) {
+func ShowAllBooksAPI(ctx *gin.Context) {
 	book := model.SelectAll()
 	ctx.JSON(200, gin.H{"book": book})
 }
 
-func AddBook(ctx *gin.Context) {
+func ShowAllBooks(ctx *gin.Context) {
+	book := model.SelectAll()
+	ctx.HTML(200, "index.html", gin.H{"book": book})
+}
+
+func AddBookAPI(ctx *gin.Context) {
 	var book model.Book
 	ctx.BindJSON(&book)
+}
+
+func AddBook(ctx *gin.Context) {
+	var form model.Book
+	// ここがバリデーション部分
+	if err := ctx.Bind(&form); err != nil {
+		book := model.SelectAll()
+		ctx.HTML(http.StatusBadRequest, "index.html", gin.H{"book": book, "err": err})
+		ctx.Abort()
+	} else {
+		title := ctx.PostForm("title")
+		isbn, err := strconv.Atoi(ctx.PostForm("isbn"))
+		if err != nil {
+			book := model.SelectAll()
+			ctx.HTML(http.StatusBadRequest, "index.html", gin.H{"book": book, "err": err})
+			ctx.Abort()
+		}
+		publisher := ctx.PostForm("publisher")
+		book := model.Book{Title: title, ISBN: isbn, Publisher: publisher}
+		model.Insert(book)
+		ctx.Redirect(302, "/")
+	}
 }
--- controller/router.go
+++ controller/router.go
@@ -4,7 +4,10 @@ import "github.com/gin-gonic/gin"
 
 func GetRouter() *gin.Engine {
 	r := gin.Default()
+	r.LoadHTMLGlob("view/*.html")
 	r.GET("/", ShowAllBooks)
 	r.POST("/add", AddBook)
+	r.GET("/api", ShowAllBooksAPI)
+	r.POST("/add/api", AddBookAPI)
 	return r
 }
